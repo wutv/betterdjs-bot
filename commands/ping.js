284fb951066f39c0660fb0b3e3ec460e7985a819
avixityyt <74422918+avixityyt@users.noreply.github.com>
2021-07-06 13:00:38 +0300

    small change
    yeah why not lol

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,12 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	run async(message, client, args) {
+		const { MessageEmbed } = require('discord.js')
+		const bald = MessageEmbed()
+		.setTitle('Pong')
+        	.addField(`> WS Latency` , `\`\`\`${Math.round(client.ws.ping)}ms\`\`\``)
+		.addField(`> Bot Latency` , `\`\`\`${Date.now() - message.createdTimestamp}ms\`\`\``)
+		.setFooter(`${message.guild.name}` , message.author.avatarURL({ dynamic: true }))
 		message.channel.send('Pong! ' + client.ws.ping + 'ms');
 	},
 };

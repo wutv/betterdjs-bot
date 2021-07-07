d83ddf8c6bcccc31fea368c383471a302f4101b7
DottoArts <80960175+DottoArts@users.noreply.github.com>
2021-07-07 11:17:01 +0200

    Fixed the ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,11 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	async run(message, client, args) {
-		const { MessageEmbed } = require('discord.js')
-		const bald = MessageEmbed()
+		const pingEmbed = new Discord.MessageEmbed()
 		.setTitle('Pong')
         	.addField(`> WS Latency` , `\`\`\`${Math.round(client.ws.ping)}ms\`\`\``)
 		.addField(`> Bot Latency` , `\`\`\`${Date.now() - message.createdTimestamp}ms\`\`\``)
 		.setFooter(`${message.guild.name}` , message.author.avatarURL({ dynamic: true }))
-		message.channel.send('Pong! ' + client.ws.ping + 'ms');
+		message.channel.send(pingEmbed);
 	},
 };

6b5ae29e7457b5f48d78d19db07aa9a19f68d1ee
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-07-07 10:24:05 -0700

    Update threads.js

--- commands/threads.js
+++ commands/threads.js
@@ -1,11 +1,6 @@
- const Discord = require('discord.js');
+const Discord = require('discord.js');
 const {Client , MessageEmbed} = require('discord.js');
-var hello = "client.on('message', message => {
-	if (message.content === '!thread example') {
-  message.startThread(Thread Example, 10m, [Testing Threads with BDJS])
-	}
-});
-";
+
 
 module.exports = {
 	name: 'docs-threads',
@@ -13,7 +8,11 @@ module.exports = {
 	async run(message, client, args) {
      const threadsdocs = new MessageEmbed()
      .setTitle('BDJS Threads')
-     .setDescription('How to Use: message.startThread(Name, Time, [reason (optional)]) \nLink: https://betterdjs.tk/docs/threads\n', hello)
+     .setDescription('How to Use: message.startThread(Name, Time, [reason (optional)]) \nLink: https://betterdjs.tk/docs/threads')
+     .addField('Code','client.on('message', message => { if (message.content === '!thread example') { message.startThread(Thread Example, 10m, [Testing Threads with BDJS])}});', true)
      message.channel.send(threadsdocs)
 	},
 };
+
+client.on('message', message => { if (message.content === '!thread example') { message.startThread(Thread Example, 10m, [Testing Threads with BDJS])}});
+

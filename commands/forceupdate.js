af00da0233265a4d17ff8c8d344264b2aff57c2e
DottoArts <80960175+DottoArts@users.noreply.github.com>
2021-07-07 17:59:46 +0200

    Update forceupdate.js

--- commands/forceupdate.js
+++ commands/forceupdate.js
@@ -13,8 +13,8 @@ module.exports = {
                 if (response.includes("Already up to date.")) {
                     message.channel.send('Bot already up to date. No changes since last pull')
                 } else {
-                    client.channels.cache.get('862049598801444874').send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**")
-                    setTimeout(() => {
+                    client.channels.cache.get('862049598801444874').send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**" + response + "THE UPDATE WAS FORCED BY A STAFF MEMBER!")
+		    setTimeout(() => {
                         process.exit();
                     }, 1000)
                 }

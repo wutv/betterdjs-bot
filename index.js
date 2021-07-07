72454a2b1d094bb0bd45cc0f26b261bb3f983707
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-07-07 13:44:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -111,24 +111,6 @@ client.on("messageReactionAdd", async function (messageReaction, user) {
     }
 })
 
-client.on("message", message => {
-  const args = message.content.split(" ").slice(1);
-
-  if (message.content.startsWith(config.prefix + "eval")) {
-    if(channel.id !== 861698467939614750) return;
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
-});
 
 client.on("message", message => {
     if (message.content == "!close") {

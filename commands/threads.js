db23f21ebb6721c36f508aba40f957d121001ed7
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-07-07 10:25:58 -0700

    Update threads.js

--- commands/threads.js
+++ commands/threads.js
@@ -9,10 +9,7 @@ module.exports = {
      const threadsdocs = new MessageEmbed()
      .setTitle('BDJS Threads')
      .setDescription('How to Use: message.startThread(Name, Time, [reason (optional)]) \nLink: https://betterdjs.tk/docs/threads')
-     .addField('Code','client.on('message', message => { if (message.content === '!thread example') { message.startThread(Thread Example, 10m, [Testing Threads with BDJS])}});', true)
      message.channel.send(threadsdocs)
 	},
 };
-
-client.on('message', message => { if (message.content === '!thread example') { message.startThread(Thread Example, 10m, [Testing Threads with BDJS])}});
 

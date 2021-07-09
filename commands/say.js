576a534644c28bb419f32b247fe016695f334f2d
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-07-08 20:52:56 -0700

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const {Client , MessageEmbed} = require('discord.js');
+
+new Discord.Client({ disableMentions: 'everyone' });
+
+module.exports = {
+	name: 'say',
+	description: 'none',
+	async run(message, client, args) {
+  message.content - user.username
+	},
+};

8034a80257c68b60cea13c68b9c718b3999f544f
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-07-11 10:51:32 -0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,12 +1,10 @@
 const Discord = require('discord.js');
 const {Client , MessageEmbed} = require('discord.js');
 
-new Discord.Client({ disableMentions: 'everyone' });
-
 module.exports = {
 	name: 'say',
 	description: 'none',
 	async run(message, client, args) {
-  message.content - user.username
+		message.content - user.username
 	},
 };
